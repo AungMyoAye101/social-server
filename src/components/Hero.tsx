fa5ecc8b6d8bb514a4f3463b8f5dbc13a0e72911
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-19 21:27:15 +0630

    add story and hero components

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Storybar from "./ui/Storybar";
+
+const Hero = () => {
+  return (
+    <section>
+      <Storybar />
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/ui/Storybar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Avatar } from "@nextui-org/react";
+const Storybar = () => {
+  return (
+    <section className="flex gap-4 p-4 border shadow rounded max-w-96 overflow-hidden overflow-x-scroll">
+      <Avatar
+        showFallback
+        isBordered
+        color="primary"
+        name="John"
+        src="https://images.unsplash.com/broken"
+        size="lg"
+      />
+      <Avatar
+        showFallback
+        isBordered
+        color="primary"
+        name="John"
+        src="https://images.unsplash.com/broken"
+        size="lg"
+      />
+      <Avatar
+        showFallback
+        isBordered
+        color="primary"
+        name="John"
+        src="https://images.unsplash.com/broken"
+        size="lg"
+      />
+      <Avatar
+        showFallback
+        isBordered
+        color="primary"
+        name="John"
+        src="https://images.unsplash.com/broken"
+        size="lg"
+      />
+      <Avatar
+        showFallback
+        isBordered
+        color="primary"
+        name="John"
+        src="https://images.unsplash.com/broken"
+        size="lg"
+      />
+    </section>
+  );
+};
+
+export default Storybar;
