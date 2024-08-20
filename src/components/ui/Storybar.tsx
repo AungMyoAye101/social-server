2911e7a116bc24aef86a73a50a4b8e8b71ca0631
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-20 19:50:00 +0630

    add story bar

--- src/components/ui/Storybar.tsx
+++ src/components/ui/Storybar.tsx
@@ -2,47 +2,97 @@ import React from "react";
 import { Avatar } from "@nextui-org/react";
 const Storybar = () => {
   return (
-    <section className="flex gap-4 p-4 border shadow rounded min-w-40 max-w-96 overflow-hidden overflow-x-scroll">
-      <Avatar
-        showFallback
-        isBordered
-        color="primary"
-        name="John"
-        src="https://images.unsplash.com/broken"
-        size="lg"
-      />
-      <Avatar
-        showFallback
-        isBordered
-        color="primary"
-        name="John"
-        src="https://images.unsplash.com/broken"
-        size="lg"
-      />
-      <Avatar
-        showFallback
-        isBordered
-        color="primary"
-        name="John"
-        src="https://images.unsplash.com/broken"
-        size="lg"
-      />
-      <Avatar
-        showFallback
-        isBordered
-        color="primary"
-        name="John"
-        src="https://images.unsplash.com/broken"
-        size="lg"
-      />
-      <Avatar
-        showFallback
-        isBordered
-        color="primary"
-        name="John"
-        src="https://images.unsplash.com/broken"
-        size="lg"
-      />
+    <section className="overflow-scroll rounded shadow px-4 py-2 border border-gray-200 scroll-bar">
+      <div className="flex gap-6">
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+        <div className="space-y-2">
+          <Avatar
+            showFallback
+            isBordered
+            color="primary"
+            name="John"
+            src="https://images.unsplash.com/broken"
+            size="lg"
+          />
+          <h2 className="text-lg font-semibold ">Jhony</h2>
+        </div>
+      </div>
     </section>
   );
 };
