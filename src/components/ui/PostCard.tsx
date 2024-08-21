a7c984345f309e4d1695d43d64b9cadb5a611116
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-21 21:43:44 +0630

    fix-design

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -7,6 +7,8 @@ import {
   Button,
 } from "@nextui-org/react";
 import Image from "next/image";
+import Image5 from "@/components/img/userPhoto.jpg";
+import Image1 from "@/components/img/image 11.jpg";
 
 const PostCard = () => {
   return (
@@ -22,19 +24,15 @@ const PostCard = () => {
             />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-default-600">
-                Zoey Lang
+                Chris Evan
               </h4>
             </div>
           </div>
         </CardHeader>
         <div className="px-4 flex flex-col gap-2">
-          <Image
-            src={""}
-            alt="photo"
-            width={160}
-            height={200}
-            className="rounded"
-          />
+          <div className="relative w-full h-40 sm:h-56 md:h-60 lg:h-52">
+            <Image src={Image1} alt="photo" fill className="rounded" />
+          </div>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
             adipisci earum architecto quasi velit nulla aliquid reiciendis
@@ -64,19 +62,15 @@ const PostCard = () => {
             />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-default-600">
-                Zoey Lang
+                Chris Evan
               </h4>
             </div>
           </div>
         </CardHeader>
         <div className="px-4 flex flex-col gap-2">
-          <Image
-            src={""}
-            alt="photo"
-            width={160}
-            height={200}
-            className="rounded"
-          />
+          <div className="relative w-full h-40 sm:h-56 md:h-60 lg:h-52">
+            <Image src={Image1} alt="photo" fill className="rounded" />
+          </div>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
             adipisci earum architecto quasi velit nulla aliquid reiciendis
@@ -106,19 +100,15 @@ const PostCard = () => {
             />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-default-600">
-                Zoey Lang
+                Chris Evan
               </h4>
             </div>
           </div>
         </CardHeader>
         <div className="px-4 flex flex-col gap-2">
-          <Image
-            src={""}
-            alt="photo"
-            width={160}
-            height={200}
-            className="rounded"
-          />
+          <div className="relative w-full h-40 sm:h-56 md:h-60 lg:h-52">
+            <Image src={Image1} alt="photo" fill className="rounded" />
+          </div>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
             adipisci earum architecto quasi velit nulla aliquid reiciendis
@@ -148,61 +138,15 @@ const PostCard = () => {
             />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-default-600">
-                Zoey Lang
+                Chris Evan
               </h4>
             </div>
           </div>
         </CardHeader>
         <div className="px-4 flex flex-col gap-2">
-          <Image
-            src={""}
-            alt="photo"
-            width={160}
-            height={200}
-            className="rounded"
-          />
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
-            adipisci earum architecto quasi velit nulla aliquid reiciendis
-            dolorum, odit nesciunt minima corrupti. Praesentium reiciendis
-            voluptatum numquam qui sint error blanditiis.
-          </p>
-        </div>
-        <CardFooter className="gap-3">
-          <div className="flex gap-1">
-            <p className="font-semibold text-default-400 text-small">4</p>
-            <p className=" text-default-400 text-small">Following</p>
+          <div className="relative w-full h-40 sm:h-56 md:h-60 lg:h-52">
+            <Image src={Image1} alt="photo" fill className="rounded" />
           </div>
-          <div className="flex gap-1">
-            <p className="font-semibold text-default-400 text-small">97.1K</p>
-            <p className="text-default-400 text-small">Followers</p>
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="">
-        <CardHeader className="justify-between">
-          <div className="flex gap-5">
-            <Avatar
-              isBordered
-              radius="full"
-              size="md"
-              src="https://nextui.org/avatars/avatar-1.png"
-            />
-            <div className="flex flex-col gap-1 items-start justify-center">
-              <h4 className="text-small font-semibold leading-none text-default-600">
-                Zoey Lang
-              </h4>
-            </div>
-          </div>
-        </CardHeader>
-        <div className="px-4 flex flex-col gap-2">
-          <Image
-            src={""}
-            alt="photo"
-            width={160}
-            height={200}
-            className="rounded"
-          />
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
             adipisci earum architecto quasi velit nulla aliquid reiciendis
--- src/components/ui/ads.tsx
+++ src/components/ui/ads.tsx
@@ -1,21 +1,31 @@
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import React from "react";
+import Image1 from "@/components/img/image 11.jpg";
+import Image5 from "@/components/img/userPhoto.jpg";
 
 const ads = () => {
   return (
     <section className="flex flex-col gap-4 rounded shadow-md p-4 border border-gray-100">
       <h1 className="font-sans text-md font-semibold">Sponered Ads</h1>
       <div className="flex flex-col gap-2">
-        <Image
-          width={160}
-          height={200}
-          alt="sponered ads"
-          src={"/"}
-          className="aspect-video rounded"
-        />
-        <div className="flex gap-2">
-          <Image width={20} height={20} alt="sponered ads" src={"/"} />
+        <div className="relative w-full h-36 rounded-md">
+          <Image
+            fill
+            alt="sponered ads"
+            src={Image1}
+            className="aspect-video rounded"
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="relative size-8 ">
+            <Image
+              alt="sponered ads"
+              src={Image5}
+              fill
+              className="object-cover rounded-full"
+            />
+          </div>
           <h3 className="text-sm font-semibold text-primary-400">Sponser</h3>
         </div>
         <div>
