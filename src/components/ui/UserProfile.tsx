15b655bd853eb848ff39100ad1339d19a40fb164
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-22 13:49:41 +0630

    add user profile

--- src/components/ui/UserProfile.tsx
+++ src/components/ui/UserProfile.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import Image2 from "@/components/img/image 7.jpg";
+import { Button } from "@nextui-org/react";
 
 const UserProfile = () => {
   return (
-    <section className="flex flex-col gap-4 rounded shadow-md ">
+    <section className="flex flex-col gap-2 rounded shadow-md ">
       <div className="w-full h-24 relative">
         <Image
           src={Image2}
@@ -19,6 +20,18 @@ const UserProfile = () => {
           alt="user cover photo"
           className="size-10 ring-2 ring-gray-100 rounded-full absolute left-0 right-0 -bottom-3 mx-auto object-cover"
         />
+      </div>
+      <div className="flex flex-col  p-2">
+        <h1 className="font-serif text-lg font-semibold text-center">
+          Chris Evan
+        </h1>
+        <p className="text-sm font-thin text-center">
+          <span className="font-semibold">1M </span>
+          Followers
+        </p>
+        <Button size="sm" color="primary">
+          My Profile
+        </Button>
       </div>
     </section>
   );
