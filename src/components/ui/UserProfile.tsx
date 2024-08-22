cf795f3faa13d17626a19beb6d8b082765890b80
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-22 13:42:04 +0630

    add user profile

--- src/components/ui/UserProfile.tsx
+++ src/components/ui/UserProfile.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
 import React from "react";
+import Image2 from "@/components/img/image 7.jpg";
 
 const UserProfile = () => {
   return (
-    <section className="flex flex-col gap-4 rounded shadow-md p-4">
-      hello
+    <section className="flex flex-col gap-4 rounded shadow-md ">
+      <div className="w-full h-24 relative">
+        <Image
+          src={Image2}
+          fill
+          alt="user cover photo"
+          className="rounded object-cover"
+        />
+        <Image
+          src={Image2}
+          width={40}
+          height={40}
+          alt="user cover photo"
+          className="size-10 ring-2 ring-gray-100 rounded-full absolute left-0 right-0 -bottom-3 mx-auto object-cover"
+        />
+      </div>
     </section>
   );
 };
