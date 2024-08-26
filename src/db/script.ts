042ee2bbf5d7b9b8c79cb274d45cf05979c5d5aa
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-26 19:21:39 +0630

    test prisma

--- src/db/script.ts
+++ src/db/script.ts
@@ -3,14 +3,21 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const user = await prisma.user.create({
+    data: {
+      name: "Harry",
+      email: "harry@prisma.io",
+    },
+  });
+  console.log(user);
 }
 
 main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
