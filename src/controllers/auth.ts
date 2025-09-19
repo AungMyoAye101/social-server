4254a7168469275bb39094645b2a10a4f08cb55e
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-09-19 12:49:29 +0630

    Add: logout function and router.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -14,10 +14,15 @@ export const register = async (req: Request, res: Response) => {
         const hashed_password = await bcrypt.hash(password, 10)
         const newUser = await User.create({ password: hashed_password, username, email })
 
-        jwt.sign({ userId: newUser._id, role: newUser.role }, process.env.JWT_SECRET_KEY as string, {
+        const token = jwt.sign({ userId: newUser._id, role: newUser.role }, process.env.JWT_SECRET_KEY as string, {
             expiresIn: '24h'
         })
-
+        res.cookie("acess_token", token, {
+            httpOnly: true,
+            maxAge: 1 * 24 * 60 * 60 * 1000, //24 hour
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "lax" : "none"
+        })
         return res.status(201).json({ message: 'User created successful.', user: newUser })
     } catch (error) {
         console.log(error)
@@ -40,11 +45,27 @@ export const login = async (req: Request, res: Response) => {
             return res.status(400).json({ message: 'Incorrect password!' })
         }
 
-        jwt.sign({ userId: userExit._id, role: userExit.role }, process.env.JWT_SECRET_KEY as string, { expiresIn: "24h" })
-
+        const token = jwt.sign({ userId: userExit._id, role: userExit.role }, process.env.JWT_SECRET_KEY as string, { expiresIn: "24h" })
+        res.cookie("acess_token", token, {
+            httpOnly: true,
+            maxAge: 1 * 24 * 60 * 60 * 1000, //24 hour
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "lax" : "none"
+        })
         return res.status(200).json({ message: 'Login successfull.', user: userExit })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: 'Internal server error!' })
     }
+}
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        res.clearCookie("access_token")
+        return res.status(200).json({ message: 'User logout' })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error!' })
+    }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 import express from "express"
 import { Request, Response } from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route"
+import cors from "cors"
 dotenv.config()
 
 const port = process.env.PORT || 5000
@@ -9,8 +12,12 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(cookieParser())
+app.use(cors({
+    credentials: true
+}))
 
-
+app.use('/api/v1/auth', authRouter)
 app.get('/', (req: Request, res: Response) => {
     res.send("sever is running..")
 })
