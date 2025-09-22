f3fedbf29f319c541de59264a7fd9d01bdb526e0
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-09-22 21:59:48 +0630

    ADD : Follow and unfollow features.

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import mongoose from "mongoose"
 import User from "../models/user.model"
+import { JwtPayload } from "jsonwebtoken"
+import { AuthRequest } from "../types"
 
 //get user by id
 export const getUserById = async (req: Request, res: Response) => {
@@ -42,3 +44,80 @@ export const getAllUsers = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Internal server error." })
     }
 }
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { userId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: "Invalid userId!" })
+    }
+
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, req.body, { new: true }).select('-password')
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found ." })
+        }
+        return res.status(200).json({ message: "user updated", user })
+
+    } catch (error) {
+        console.warn(error)
+        return res.status(500).json({ message: "Internal server error." })
+    }
+}
+
+export const toFollow = async (req: AuthRequest, res: Response) => {
+    const { userId } = req.user as JwtPayload
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid userId!" })
+    }
+    if (userId === id) {
+        return res.status(400).json({ message: "You can't follow yourself" })
+    }
+    try {
+        const followUser = await User.findByIdAndUpdate(id, {
+            $addToSet: { followers: userId }
+        }, { new: true })
+        const user = await User.findByIdAndUpdate(userId, {
+            $addToSet: { following: id }
+        }, { new: true })
+
+        if (!user || !followUser) {
+            return res.status(404).json({ message: "User not found." })
+        }
+
+        return res.status(200).json({ mesage: 'following user success', })
+    } catch (error) {
+        console.warn(error)
+        return res.status(500).json({ message: "Internal server error." })
+    }
+}
+
+export const unFollowUser = async (req: AuthRequest, res: Response) => {
+    const { userId } = req.user as JwtPayload
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid userId!" })
+    }
+    if (userId == - id) {
+        return res.status(400).json({ message: "You can't unfollow yourself." })
+    }
+    try {
+        const user = await User.findOne({ _id: id })
+        if (!user) {
+            return res.status(404).json({ message: "User not found." })
+        }
+        if (!user.followers.includes(userId)) {
+            return res.status(400).json({ message: "You are not following user" })
+        }
+        await User.findByIdAndUpdate(id, { $pull: { follower: userId } }, { new: true })
+        await User.findByIdAndUpdate(userId, { $pull: { following: id } }, { new: true })
+        return res.status(200).json({ mesage: 'unFlowing success' })
+    } catch (error) {
+        console.warn(error)
+        return res.status(500).json({ message: "Internal server error." })
+    }
+}
+
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,13 @@ import express from "express"
 import { verifyUser } from "../middleware/verifyUser"
 import { Request, Response } from "express"
 import { AuthRequest } from "../types"
-import { getAllUsers, getUserById } from "../controllers/user"
+import { getAllUsers, getUserById, toFollow, unFollowUser } from "../controllers/user"
 
 const userRouter = express.Router()
 
 userRouter.get("/allusers", getAllUsers) //get all user and pagination
 userRouter.get('/:userId', getUserById) //get user by id 
+userRouter.post("/follow/:id", verifyUser, toFollow)
+userRouter.post("/unfollow/:id", verifyUser, unFollowUser)
 
 export default userRouter
