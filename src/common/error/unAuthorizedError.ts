a78e3776336ee6f89d495df0e50b8776895de2cb
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-21 22:47:06 +0630

    Feat : Add custom Error .

---
+++ src/common/error/unAuthorizedError.ts
@@ -0,0 +1,12 @@
+import { CustomError } from "./customError";
+
+export class UnauthorizedError extends CustomError {
+    statusCode = 401;
+    constructor(public message: string) {
+        super(message)
+    }
+
+    generateErrors() {
+        return [{ message: this.message }]
+    }
+}
